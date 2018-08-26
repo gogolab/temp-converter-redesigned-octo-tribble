00862f73e23e8876134bcc46b623400bdf4e148d
gogolab <m4ttek@gmail.com>
2018-08-26 19:20:04 +0200

    form change updates model

--- src/Update.js
+++ src/Update.js
@@ -1,7 +1,92 @@
-import * as R from 'ramda';
+import * as R from "ramda";
 
-function update (msg, model) {
-  return model;
+const MSGS = {
+    LEFT_VALUE_INPUT: "LEFT_VALUE_INPUT",
+    RIGHT_VALUE_INPUT: "RIGHT_VALUE_INPUT",
+    LEFT_UNIT_SELECT: "LEFT_UNIT_SELECT",
+    RIGHT_UNIT_SELECT: "RIGHT_UNIT_SELECT"
+};
+
+export function leftInputMsg(leftValue) {
+    return {
+        type: MSGS.LEFT_VALUE_INPUT,
+        leftValue
+    };
+}
+
+export function rightInputMsg(rightValue) {
+    return {
+        type: MSGS.RIGHT_VALUE_INPUT,
+        rightValue
+    };
+}
+
+export function leftUnitSelectMsg(leftUnit) {
+    return {
+        type: MSGS.LEFT_UNIT_SELECT,
+        leftUnit
+    };
+}
+
+export function rightUnitSelectMsg(rightUnit) {
+    return {
+        type: MSGS.RIGHT_UNIT_SELECT,
+        rightUnit
+    };
+}
+
+const toInt = R.pipe(
+    parseInt,
+    R.defaultTo(0)
+);
+
+function update(msg, model) {
+    switch (msg.type) {
+        case MSGS.LEFT_VALUE_INPUT:
+            if (msg.leftValue === "") {
+                return {
+                    ...model,
+                    sourceLeft: true,
+                    leftValue: "",
+                    rightValue: ""
+                };
+            }
+            const leftValue = toInt(msg.leftValue);
+            return {
+                ...model,
+                sourceLeft: true,
+                leftValue
+            };
+        case MSGS.RIGHT_VALUE_INPUT:
+            if (msg.rightValue === "") {
+                return {
+                    ...model,
+                    sourceLeft: false,
+                    leftValue: "",
+                    rightValue: ""
+                };
+            }
+            const rightValue = toInt(msg.rightValue);
+            return {
+                ...model,
+                sourceLeft: false,
+                rightValue
+            };
+        case MSGS.LEFT_UNIT_SELECT:
+            return {
+                ...model,
+                leftUnit: msg.leftUnit
+            };
+        case MSGS.RIGHT_UNIT_SELECT:
+            return {
+                ...model,
+                rightUnit: msg.rightUnit
+            };
+
+        default:
+            console.log("bad message");
+            return model;
+    }
 }
 
 export default update;
--- src/View.js
+++ src/View.js
@@ -1,19 +1,65 @@
-import * as R from 'ramda';
-import hh from 'hyperscript-helpers';
-import { h } from 'virtual-dom';
+import * as R from "ramda";
+import hh from "hyperscript-helpers";
+import { h } from "virtual-dom";
 
-const {
-  div,
-  h1,
-  pre,
-} = hh(h);
+import {
+    leftInputMsg,
+    rightInputMsg,
+    leftUnitSelectMsg,
+    rightUnitSelectMsg
+} from "./Update";
 
+const { div, h1, pre, input, select, option } = hh(h);
+
+const units = ["Celsius", "Fahrenheit", "Kelvin"];
+
+function unitOptions(selectedUnit) {
+    return units.map(unit =>
+        option({ value: unit, selected: unit === selectedUnit }, unit)
+    );
+}
+
+function unitSection(dispatch, unit, value, inputMsg, selectMsg) {
+    return div({ className: "w-50 ma1" }, [
+        input({
+            type: "text",
+            className: "w-100",
+            value,
+            oninput: e => dispatch(inputMsg(e.target.value))
+        }),
+        select(
+            {
+                className: "w-100",
+                onchange: e => {
+                    dispatch(selectMsg(e.target.value));
+                }
+            },
+            [unitOptions(unit)]
+        )
+    ]);
+}
 
 function view(dispatch, model) {
-  return div({ className: 'mw6 center' }, [
-    h1({ className: 'f2 pv2 bb' }, 'Temperature Unit Converter'),
-    pre(JSON.stringify(model, null, 2)),
-  ]);
+    return div({ className: "mw6 center" }, [
+        h1({ className: "f2 pv2 bb" }, "Temperature Unit Converter"),
+        div({ className: "flex" }, [
+            unitSection(
+                dispatch,
+                model.leftUnit,
+                model.leftValue,
+                leftInputMsg,
+                leftUnitSelectMsg
+            ),
+            unitSection(
+                dispatch,
+                model.rightUnit,
+                model.rightValue,
+                rightInputMsg,
+                rightUnitSelectMsg
+            )
+        ]),
+        pre(JSON.stringify(model, null, 2))
+    ]);
 }
 
 export default view;
